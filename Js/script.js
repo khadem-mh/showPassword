4229dfb16bc29b6894e92e32cf58844c5ee9b2d1
khadem-mh <khadem13359@gmail.com>
2024-04-21 22:07:13 +0330

    init project

---
+++ Js/script.js
@@ -0,0 +1,26 @@
+﻿// Functions for select elems with id & class
+let $ = document;
+function _id(id_name) {
+  return $.getElementById(id_name);
+}
+
+function _class(class_name) {
+  return $.getElementsByClassName(class_name);
+}
+
+// Select Elems
+let togglePassword = _class("toggle-password");
+let passwordField = _id("password-field");
+
+// Fire click event on eye icon
+//  console.log(togglePassword);
+togglePassword[0].addEventListener("click", function () {
+  //  console.log('Run');
+  if (passwordField.type == "text") {
+    passwordField.type = "password";
+    togglePassword[0].classList.remove("active");
+  } else {
+    passwordField.type = "text";
+    togglePassword[0].classList.add("active");
+  }
+})
